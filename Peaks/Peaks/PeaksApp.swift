03dba50a5be143fceb255de8fd5a9e4735149160
Lukasz Knap <lukaszkn@go2.pl>
2024-05-07 17:55:32 +0200

    Initial project setup

---
+++ Peaks/Peaks/PeaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeaksApp.swift
+//  Peaks
+//
+//  Created by Lukasz on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
